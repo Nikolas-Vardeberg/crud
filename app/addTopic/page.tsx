88b72ee87c53bb01754406fff735b819a28044d9
niklusrook <post@nikolas.no>
2024-01-21 10:52:00 +0100

    Create task

--- app/addTopic/page.tsx
+++ app/addTopic/page.tsx
@@ -1,20 +1,48 @@
-import React from 'react'
+"use client"
+
+import { useState } from "react"
+
 
 const page = () => {
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+
+  const handleSubmit = async () => {
+
+    if (!title || !description) {
+      alert("Title and description are required");
+      return;
+    }
+
+    try {
+      const res = await fetch("http://localhost:3000/api/topics", {
+        method: "POST",
+        headers: {
+          "Content-type": "application/json",
+        },
+        body: JSON.stringify({ title, description })
+      })
+    } catch (error) {}
+  }
+
   return (
-    <form className='flex flex-col gap-3'>
+    <form onSubmit={handleSubmit} className='flex flex-col gap-3'>
       <input
+        onChange={(e) => setTitle(e.target.value)}
+        value={title}
         className='border border-slate-500 px-8 py-2'
         type='text'
         placeholder='Topic Title'
       />
 
     <input
+        onChange={(e) => setDescription(e.target.value)}
+        value={description}
         className='border border-slate-500 px-8 py-2'
         type='text'
         placeholder='Topic Description'
       />
-      <button className='bg-green-600 font-bold text-white py-3 px-6 w-fit'>Add Topic</button>
+      <button type="submit" className='bg-green-600 font-bold text-white py-3 px-6 w-fit'>Add Topic</button>
     </form>
   )
 }
--- components/TopicList.tsx
+++ components/TopicList.tsx
@@ -2,23 +2,53 @@ import Link from "next/link";
 import RemoveBtn from "./RemoveBtn";
 import { HiPencilAlt } from 'react-icons/hi'
 
+const getTopics = async (): Promise<{ topics: Topic[] }> => {
+    try {
+        const res = await fetch("http://localhost:3000/api/topics", {
+            cache: "no-store", 
+        });
 
-export default function TopicList() {
-    return(
-        <>
-            <div className="p-4 border border-slate-300 my-3 flex justify-between gap-5 items-start">
-                <div>
-                    <h2 className="font-bold text-2xl">Topic Title</h2>
-                    <div>Topic Description</div>
-                </div>
+        if (!res.ok) {
+            throw new Error("Failed to fetch topics");
+        }
 
-                <div className="flex gap-2">
-                    <RemoveBtn />
-                    <Link href="/addTopic">
-                        <HiPencilAlt size={24}/>
-                    </Link>
-                </div>
-            </div>
-        </>
-    )
+        return res.json();
+    } catch (error) {
+        console.error("Error loading topics", error);
+        // Handle the error or rethrow it if needed
+        throw error;
+    }
+};
+
+interface Topic {
+    _id: number;
+    title: string;
+    description: string;
+    // Add any other properties of your topic object here
 }
+
+const TopicList  = async () => {
+    const { topics } = await getTopics();
+
+    return (
+        <>
+            {topics.map((t) => (
+                <div className="p-4 border border-slate-300 my-3 flex justify-between gap-5 items-start">
+                    <div>
+                        <h2 className="font-bold text-2xl">{t.title}</h2>
+                        <div>{t.description}</div>
+                    </div>
+
+                    <div className="flex gap-2">
+                        <RemoveBtn />
+                        <Link href={`/editTopic/${t._id}`}>
+                            <HiPencilAlt size={24}/>
+                        </Link>
+                    </div>
+                </div>
+            ))}
+        </>
+    );
+};
+
+export default TopicList;
